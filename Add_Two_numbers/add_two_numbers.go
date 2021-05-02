13dfa6a12e53fdefebd117e2a01a2ef78e7cf21b
sleepyfox97 <h1649bot@gmail.com>
2021-05-02 09:49:57 +0900

    long time no see go

--- Add_Two_numbers/add_two_numbers.go
+++ Add_Two_numbers/add_two_numbers.go
@@ -1,25 +1,32 @@
-func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-    re := &ListNode{}
-    mre := re
-    re_mvup := 0
+package main
 
-    re_mvup = 0
-    for l1 != nil || l2 != nil {
-        re_val := re_mvup
-        if l1 != nil {
-            re_val += l1.Val
-            l1 = l1.Next
-        }
-        if l2 != nil {
-            re_val += l2.Val
-            l2 = l2.Next
-        }
-        re_mvup = re_val / 10
-        mre.Next = &ListNode{Val: re_val % 10}
-        mre = mre.Next
-    }
-    if re_mvup != 0 {
-        mre.Next = &ListNode{Val: 1}
-    }
-    return re.Next
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	re := &ListNode{}
+	mre := re
+	re_mvup := 0
+
+	re_mvup = 0
+	for l1 != nil || l2 != nil {
+		re_val := re_mvup
+		if l1 != nil {
+			re_val += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			re_val += l2.Val
+			l2 = l2.Next
+		}
+		re_mvup = re_val / 10
+		mre.Next = &ListNode{Val: re_val % 10}
+		mre = mre.Next
+	}
+	if re_mvup != 0 {
+		mre.Next = &ListNode{Val: 1}
+	}
+	return re.Next
 }
